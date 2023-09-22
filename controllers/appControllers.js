29f3bd75b6a5ab6e971a4c45f2f8b54b8773c5f8
venkadesh004 <venkadeshdeveloper@gmail.com>
2023-09-23 02:14:46 +0530

    Initial commit

--- controllers/appControllers.js
+++ controllers/appControllers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const fileUrl = "./application/layoutFixed.apk";
+const fileUrl = "./application/PlanetsAR.apk";
 
 const downloadApp = (req, res) => {
     return res.download(fileUrl);
